e166042455615e927aa68aed528fb7a7691a252a
Antony Sanders <antony.devon92@gmail.com>
2020-01-16 01:35:15 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,13 @@
+class Song 
+  attr_accessor :title,:artist 
+  
+  # def initialize 
+  #   @title = title
+  #   @author = author 
+  # end 
+  
+  def self.artist  
+    self.Artist  = artist 
+  end 
+  
+end 
